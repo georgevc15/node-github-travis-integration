89685311d922ab3485cef61c42d64f49ea8cbaa2
George Codat Tudoran <office@solcreation.ro>
2016-11-07 20:10:53 +0200

    Added webhooks

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ app.use(require('body-parser').urlencoded({extended:true}));
 app.use(require('body-parser').json());
 
 app.use(Express.static('./app'));
+
 app.get('/messages',(req,res)=>{
-	res.status(200).json(require('./messages.json'))
-})
+	res.status(200).json(require('./messages.json'));
+});
+
 app.listen(port,()=>{
 	//console.log(`Running webhook listener...`);
 	console.log(`App listening on port ${port}`);
 }) ;
 
-require('./webhook')();
+require('./webhook')(app);
